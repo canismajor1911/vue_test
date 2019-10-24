0daf1ee4e94e2bd975cf932609b10ee9a800234e
wjatek <panmcciastek@gmail.com>
2019-10-24 21:53:27 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <CommentList />
+    </div>
+</template>
+
+<script>
+    import CommentList from './components/CommentList';
+
+    export default {
+        name: 'app',
+        components: {
+            CommentList
+        }
+    }
+
+</script>
+
+<style>
+    * {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: Arial, Helvetica, sans-serif;
+        line-height: 1.5;
+    }
+</style>
---
+++ src/components/CommentList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello, I'm CommentList!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CommentList"
+    }
+</script>
+
+<style scoped>
+
+</style>
