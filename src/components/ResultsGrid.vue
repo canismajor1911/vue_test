748f0962d0bb6adef39f570d8bc46a162a2ad109
wjatek <panmcciastek@gmail.com>
2019-10-25 22:19:08 +0200

    Add information text to results grid

--- src/components/ResultsGrid.vue
+++ src/components/ResultsGrid.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="wrapper">
-        <Thumbnail v-for="(drink, id) in data" :drink="drink" :key="id" />
+        <p>Here you go! Click on the cocktail that you're interested in to see more details.</p>
+        <hr>
+        <div class="grid">
+            <Thumbnail v-for="(drink, id) in data" :drink="drink" :key="id" />
+        </div>
     </div>
 </template>
 
@@ -24,6 +28,19 @@
     @import "../styles/settings.scss";
 
     .wrapper {
+        p {
+            text-align: center;
+            margin: 1em 0 0;
+        }
+
+        hr {
+            border: 0;
+            border-bottom: 1px solid #DDD;
+            margin: 1em;
+        }
+    }
+
+    .grid {
         display: grid;
         grid-template-columns: 1fr 1fr;
 
